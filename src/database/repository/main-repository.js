d1a346a22d3a179d2fa886eb7747eadc4337b035
Paul Barasa <65596472+kpbarasa@users.noreply.github.com>
2023-01-17 09:12:22 +0300

    git checkout -b feat-2-update-apis-services-repositories (#2)

--- src/database/repository/main-repository.js
+++ src/database/repository/main-repository.js
@@ -1,9 +1,63 @@
+const { testDataModel } = require('../models')
 
 class MainRepository {
 
-    async RepositoryFunction (){
+    async RepositoryFunction() {
         return "service is operational";
     }
+
+    async GetData() {
+
+        const data = await testDataModel.find()
+
+        return data
+
+    }
+
+    async GetDataById(service_data) {
+        const { _id } = service_data;
+        const data = await testDataModel.findById(_id)
+
+        return data
+
+    }
+
+    async PostData(service_data) {
+
+        const { _id, req_body } = service_data;
+
+        const payload = { req_body }
+
+        const data = await testDataModel.create(payload)
+
+        return data
+    }
+
+    async UpdateData(service_data) {
+
+        const { _id, req_body } = service_data;
+
+        const payload = { req_body }
+
+        const data = await testDataModel.findByIdAndUpdate({ _id }, payload,
+            
+            {
+                new: true,
+            }
+        )
+
+        return data
+    }
+
+    async DeleteData(service_data) { 
+
+        const { _id } = service_data;
+
+        const data = await testDataModel.findByIdAndDelete({ _id })
+
+        return data
+    }
+
 };
 
 module.exports = MainRepository;
