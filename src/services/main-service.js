d4241f002ae002ba3ce0df8267804a9246be6114
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2023-01-17 09:13:45 +0300

    git checkout -b feat-3-update-apis-services-repositories

--- src/services/main-service.js
+++ src/services/main-service.js
@@ -20,6 +20,76 @@ class mainService {
         return FormateData(payload)
 
     };
+
+    async GetData(api_data) {
+
+        const response_data = await this.repository.GetData(api_data)
+
+        const payload = {
+            message:"success", 
+            status:"404" ,
+            response_data, 
+        };
+
+        return FormateData(payload)
+
+    };
+
+    async GetDataById(api_data) {
+
+        const response_data = await this.repository.RepositoryFunction(api_data)
+
+        const payload = {
+            message:"success", 
+            status:"404" ,
+            response_data, 
+        };
+
+        return FormateData(payload)
+
+    };
+
+    async PostData(api_data) {
+
+        const response_data = await this.repository.RepositoryFunction(api_data)
+
+        const payload = {
+            message:"success", 
+            status:"404" ,
+            response_data, 
+        };
+
+        return FormateData(payload)
+
+    };
+
+    async UpdateData(api_data) {
+
+        const response_data = await this.repository.RepositoryFunction(api_data)
+
+        const payload = {
+            message:"success", 
+            status:"404" ,
+            response_data, 
+        };
+
+        return FormateData(payload)
+
+    };
+
+    async DeleteData(api_data) {
+
+        const response_data = await this.repository.RepositoryFunction(api_data)
+
+        const payload = {
+            message:"success", 
+            status:"404" ,
+            response_data, 
+        };
+
+        return FormateData(payload)
+
+    };
 };
 
 module.exports = mainService;
