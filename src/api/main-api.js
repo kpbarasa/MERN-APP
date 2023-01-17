8d2a34105cab5f030fa755361f7da5d5b5e5d5ac
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2023-01-17 09:08:43 +0300

    git checkout -b feat-2-update-apis-services-repositories

--- src/api/main-api.js
+++ src/api/main-api.js
@@ -1,4 +1,4 @@
-const MainService  = require('../services/main-service')
+const MainService = require('../services/main-service')
 
 module.exports = (app) => {
 
@@ -6,9 +6,112 @@ module.exports = (app) => {
 
     app.get('/', async (req, res, next) => {
 
-        const data = await service.MainServiceFunction();
+        try {
 
-        res.json(data);
+            const data = await service.MainServiceFunction();
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    });
+
+    app.get('/get/data', async (req, res, next) => {
+
+        try {
+
+            const data = await service.GetData();
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    });
+
+    app.get('/get/data/:id', async (req, res, next) => {
+
+        try {
+
+            const _id = req.params.id;
+
+            const data = await service.GetDataById({ _id });
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    });
+
+
+    app.post('/post/data', async (req, res, next) => {
+
+        try {
+
+            const _id = "0889678hjah8"
+
+            const api_data = { _id, req_body: req.body };
+
+            const data = await service.PostData(api_data);
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    });
+
+    app.post('/update/data/:id', async (req, res, next) => {
+
+        try {
+
+            const _id = req.params.id;
+
+            const api_data = { _id, req_body: req.body };
+
+            const data = await service.UpdateData(api_data);
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    });
+
+    app.get('/del/data', async (req, res, next) => {
+
+        try {
+
+            const _id = req.params.id;
+
+            const api_data = { _id };
+
+            const data = await service.DeleteData(api_data);
+
+            res.json(data);
+
+        } catch (error) {
+
+            next(error);
+
+        }
 
     });
 
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,1 +1,3 @@
-module.exports = {}
+module.exports = {
+    testDataModel: require('./test.model')
+}
